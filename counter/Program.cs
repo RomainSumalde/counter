3006f0b5d8405394b24237bda360f255f5c2f8c0
Romain <2019sha01186@iacademy.edu.ph>
2021-04-13 18:05:29 +0800

    final

---
+++ counter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For loop that counts from 1 to 10");
+            int a = 10;
+            for (int x = 1; x <= a; x++)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("\nFor loop that counts from 10 to 1");
+            int b = 1;
+            for (int y = 10; y >= b; y--)
+            {
+                Console.WriteLine(y);
+            }
+            Console.ReadLine();
+        }
+    }
+}
